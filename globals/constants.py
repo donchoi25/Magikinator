698c457d8ff8b21acf190358b08c957c8790880e
Don Choi <donchoi25@gmail.com>
2023-08-27 08:01:24 -0700

    Using more efficient key to cache calculations. 1000 cards now take <10 secs

--- globals/constants.py
+++ globals/constants.py
@@ -2,8 +2,17 @@ import pandas as pd
 
 cardcsv_dataframe = pd.read_csv('./data/files/cardsdata_live.csv')
 
+TOTAL_CARDS_FINAL = len(cardcsv_dataframe.index)
+
 cardcsv_dataframe.index = cardcsv_dataframe["Name"]
 
-TOTAL_CARDS_FINAL = len(cardcsv_dataframe.index)
+cardcsv_dataframe.loc["Sum"] = cardcsv_dataframe.sum().transpose()
+
+cardcsv_dataframe = cardcsv_dataframe.to_dict()
+
+print("initialization done")
+
 POSSIBLE_ANSWERS_FINAL = ["yes", "no", "maybe"]
 QUESTION_LIMIT_FINAL = 10
+
+CARD_DATA_FINAL = list(cardcsv_dataframe["Name"])[0:-1]
