39f9fc100073df4aad1d9598369da86cad56d0c8
Don Choi <donchoi25@gmail.com>
2023-08-26 18:19:18 -0700

    changed loc to at for accessing excel with pandas to improve time

--- globals/constants.py
+++ globals/constants.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 cardcsv_dataframe = pd.read_csv('./data/files/cardsdata_live.csv')
 
+cardcsv_dataframe.index = cardcsv_dataframe["Name"]
+
 TOTAL_CARDS_FINAL = len(cardcsv_dataframe.index)
 POSSIBLE_ANSWERS_FINAL = ["yes", "no", "maybe"]
 QUESTION_LIMIT_FINAL = 10
