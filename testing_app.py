e61d76aeb12e3a59cd3c347a1f9beb921d610ecf
Daniel Im <imdanieljae@gmail.com>
2023-12-07 15:45:17 -0800

    Added testing file to save progress on running test to separate text file

--- testing_app.py
+++ testing_app.py
@@ -1,40 +1,84 @@
 from frontend import frontendapi
-from globals.constants import cardcsv_dataframe
+from tabulate import tabulate
+from globals.constants import cardcsv_dataframe as df
+import os.path
+
+# Files to save test data to
+TEST_CARDS_PATH = "./data/files/test/simulate_game_allcards.txt"
+LAST_CARD_PATH = "./data/files/test/last_card.txt"
+
+# Testing parameters
+CARD_LIMIT = 10
+HOW_OFTEN_TO_SAVE = 4
 
 # Initialize the frontEnd
 frontEnd = frontendapi.FrontEnd()
-df = cardcsv_dataframe
 question_cols = df.columns
 
 def simulateGameForAllCards(print_to_console=False):
-    CARD_LIMIT = 100
     index = 0
     failed_cards = {}
-    for name, card_row in df.iterrows():
-        print('\n')
-        print("DOING TEST RUN FOR CARD " + name)
+    continuing_card = None
+    # This gets the first line of the test file, which tracks the last card we ran the test on.
+    if os.path.exists(LAST_CARD_PATH):
+        with open(LAST_CARD_PATH, 'r') as file:
+            try:
+                firstline = file.readline().strip('\n')
+                if not df.loc[firstline].empty:
+                    print("Continuing test from card: " + firstline)
+                    continuing_card = firstline
+            except KeyError as e:
+                print(f"We did not find a card with the name {str(e)} to continue from. \n Starting test from scratch.")
+            file.close()
+    else:
+        with open(LAST_CARD_PATH, "w") as file:
+            file.write("Fury Sliver")
+            file.close()
+        print("Creating a new last_card.txt file.")
 
-        response = simulateQuestionsForCard(name, card_row)
-        success = response[2] == name
+    # If we have a valid card to continue from, we'll iterate to that card before continuing our tests.
 
-        print(f'Test run finished with a {"SUCCESS" if success else "FAILURE"} AFTER {str(len(response[0]))} QUESTIONS')
-        print('\n')
-        if print_to_console:
-            print("MAGIKINATOR GUESSED " + response[2] + " AFTER " + str(len(response[0])) + " TRIES, WHEN WE LOOKED FOR " + name)
-            print("HERE ARE THE QUESTIONS WE ASKED: ")
-            print(response[0])
-            print("HERE ARE THE ANSWERS WE GAVE: ")
-            print(response[1])
-            print("################################")
-        if index >= CARD_LIMIT:
-            return
-        index += 1
+    skip_to_continue = True
+    with open(TEST_CARDS_PATH, 'a') as file:
+        for name, card_row in df.iterrows():
+            if continuing_card != None and skip_to_continue:
+                if name.lower() == continuing_card.lower():
+                    skip_to_continue = False
+                    index += 1
+                continue
+            
+            response = simulateQuestionsForCard(name, card_row)
+            success = response[2] == name
 
-        if not success:
-            failed_cards[name] = response
+            print(f"Test #{str(index)} for {name} {'SUCCEEDED' if success else 'FAILED'} after {str(len(response[0]))} questions.\n")
+            if print_to_console:
+                print(f"Questions | Answers\n")
+                print(tabulate(zip(response[0], response[1])))
+
+            file.write('\n')
+            file.write(f"Test #{str(index)} for {name} {'SUCCEEDED' if success else 'FAILED'} after {str(len(response[0]))} questions.\n")
+            file.write(f"Questions | Answers\n")
+            file.write(tabulate(zip(response[0], response[1])))
+            file.write('\n')
+
+            index += 1
+            if index % HOW_OFTEN_TO_SAVE == 0:
+                with open(LAST_CARD_PATH, "w") as f:
+                    f.seek(0, 0)
+                    f.write(name)
+                    print("Saved progress up to " + name)
+                    f.close()
+            
+            if index >= CARD_LIMIT:
+                break
+
+        file.close()
+
+    # if not success:
+    #     failed_cards[name] = response
         
-    print(str(len(failed_cards.keys)) + " CARDS FAILED!")
-    print(failed_cards)
+    # print(str(len(failed_cards.keys)) + " CARDS FAILED!")
+    # print(failed_cards)
 
 def simulateQuestionsForCard(card_name, card_data):
     MAX_QUESTIONS_TO_ASK = 40
@@ -60,9 +104,4 @@ def getAnswerForCard(card_data, question):
             best_answer_val = answer_val
     return best_answer
         
-
-
 simulateGameForAllCards()
-
-# print(cardcsv_dataframe.columns)
-# print(cardcsv_dataframe.loc['Fury Sliver'])
