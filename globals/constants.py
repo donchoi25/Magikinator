de79be42802c813e17d437603b0d2321ae03d7e5
Daniel Im <imdanieljae@gmail.com>
2024-01-18 17:13:34 -0800

    Adjusting constants.py

--- globals/constants.py
+++ globals/constants.py
@@ -15,17 +15,31 @@ import botocore
 #     else:
 #         raise
 
-cardcsv_dataframe = pd.read_csv('./data/files/cardsdata_live.csv')
-cardcsv_dataframe.index = cardcsv_dataframe["Name"]
-cardcsv_dataframe = cardcsv_dataframe.iloc[: , 1:]
-cardcsv_dataframe = cardcsv_dataframe.astype(int) #reduces memory load by converting datatype of column values into int.
+# can we store this in a postgresql database
+CHUNKSIZE=500
+TOTAL_CARDS_FINAL = 0
+TOTAL_MAP_FINAL = {}
+QUESTION_COLUMNS = {}
+with pd.read_csv('./data/files/cardsdata_live.csv', chunksize=CHUNKSIZE) as reader:
+    for chunk in reader:
+        TOTAL_CARDS_FINAL += len(chunk)
+        for col in chunk:
+            
 
-TOTAL_CARDS_FINAL = len(cardcsv_dataframe.index)
-TOTALS_MAP_FINAL = {ele:sum / 100 for ele, sum in zip(cardcsv_dataframe.columns, cardcsv_dataframe.sum().transpose().tolist())}
-CARD_DATA_FINAL = list(cardcsv_dataframe.index)
+print(TOTAL_CARDS_FINAL)
 
-# # import os, psutil; print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-POSSIBLE_ANSWERS_FINAL = ["YES", "NO", "MAYBE"]
-QUESTION_LIMIT_FINAL = float('inf')
-QUESTION_DATA_FINAL = cardcsv_dataframe.columns
-print("initialization done")
+# cardcsv_dataframe = pd.read_csv('./data/files/cardsdata_live.csv')
+# cardcsv_dataframe.index = cardcsv_dataframe["Name"]
+# cardcsv_dataframe = cardcsv_dataframe.iloc[: , 1:]
+# cardcsv_dataframe = cardcsv_dataframe.astype(int) #reduces memory load by converting datatype of column values into int.
+
+# TOTAL_CARDS_FINAL = len(cardcsv_dataframe.index)
+# Total number of YES/NO/MAYBE for each question
+# TOTALS_MAP_FINAL = {ele:sum / 100 for ele, sum in zip(cardcsv_dataframe.columns, cardcsv_dataframe.sum().transpose().tolist())}
+# CARD_DATA_FINAL = list(cardcsv_dataframe.index)
+
+# # # import os, psutil; print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+# POSSIBLE_ANSWERS_FINAL = ["YES", "NO", "MAYBE"]
+# QUESTION_LIMIT_FINAL = float('inf')
+# QUESTION_DATA_FINAL = cardcsv_dataframe.columns
+# print("initialization done")
